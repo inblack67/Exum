22705eebc1b385bbd8f60c10f496b7b3a7319f6c
ryuzaki <ryuzaki>
2024-05-12 01:07:33 +0530

    merge routers

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,19 @@ use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/hello", get(hello_handler));
+    let all_routes = Router::new().merge(routes_hello());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("Listening on {addr}");
 
     axum::Server::bind(&addr)
-        .serve(routes_hello.into_make_service())
+        .serve(all_routes.into_make_service())
         .await
         .unwrap();
+}
+
+fn routes_hello() -> Router {
+    Router::new().route("/hello", get(hello_handler))
 }
 
 #[derive(Debug, Deserialize)]
